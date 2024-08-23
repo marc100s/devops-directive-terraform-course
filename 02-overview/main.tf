b23c7c6656d5c57a1f5080ae1d6898eb9fb740bc
marc100s <marc@ellipsi.net>
2024-08-23 18:58:32 +0200

    Hashicorp Template version 2024 August

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -2,16 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "5.63.1" #video, uses version 3 and AWS CLI v1; I suggest upgrading both to ensure credentials works, etc.
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-3"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+resource "aws_instance" "app_server" {
+  ami           = "ami-09d83d8d719da9808" #ami for region, This case, Europe region
   instance_type = "t2.micro"
+ 
+  tags = {
+    Name = "ExampleAppServerInstance" #I suggest tagging, since it is better to get things more organized / named, despite it is being destroyed
+  }
 }
