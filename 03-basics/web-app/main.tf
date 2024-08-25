eec9602a6ecc7865062612c71d97c53c2fc3989f
marc100s <marc@ellipsi.net>
2024-08-25 13:56:23 +0200

    version for my enviro webapp

--- 03-basics/web-app/main.tf
+++ 03-basics/web-app/main.tf
@@ -2,9 +2,9 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
-    key            = "03-basics/web-app/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "terraformcredentials"
+    key            = "03-basics/import-bootstrap/terraform.tfstate"
+    region         = "eu-west-3"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -12,17 +12,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "5.63.1" #video, uses version 3 and AWS CLI v1; I suggest upgrading both to ensure credentials works, etc.
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-3"
 }
 
 resource "aws_instance" "instance_1" {
-  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami             = "ami-09d83d8d719da9808" # Ubuntu 20.04 LTS // eu-west-3
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instances.name]
   user_data       = <<-EOF
@@ -30,10 +30,13 @@ resource "aws_instance" "instance_1" {
               echo "Hello, World 1" > index.html
               python3 -m http.server 8080 &
               EOF
+  tags = {
+    Name = "Instance1" #I suggest tagging, since it is better to get things more organized / named, despite it is being destroyed
+  }
 }
 
 resource "aws_instance" "instance_2" {
-  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami             = "ami-064508e7b69710843" # Ubuntu 20.04 LTS // eu-west-3
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instances.name]
   user_data       = <<-EOF
@@ -41,6 +44,9 @@ resource "aws_instance" "instance_2" {
               echo "Hello, World 2" > index.html
               python3 -m http.server 8080 &
               EOF
+  tags = {
+    Name = "Instance2" #I suggest tagging, since it is better to get things more organized / named, despite it is being destroyed
+  }
 }
 
 resource "aws_s3_bucket" "bucket" {
@@ -68,9 +74,17 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
-data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["eu-west-3a"] # Replace with your desired AZ
+  }
 }
+
 
 resource "aws_security_group" "instances" {
   name = "instance-security-group"
@@ -177,22 +191,32 @@ resource "aws_security_group_rule" "allow_alb_all_outbound" {
 
 }
 
-
 resource "aws_lb" "load_balancer" {
   name               = "web-app-lb"
   load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default_subnet.ids
+  subnets            = [data.aws_subnet.default_subnet_1.id, data.aws_subnet.default_subnet_2.id]
   security_groups    = [aws_security_group.alb.id]
-
 }
 
+data "aws_subnet" "default_subnet_1" {
+  vpc_id            = data.aws_vpc.default_vpc.id
+  availability_zone = "eu-west-3a"
+}
+
+data "aws_subnet" "default_subnet_2" {
+  vpc_id            = data.aws_vpc.default_vpc.id
+  availability_zone = "eu-west-3b"
+}
+
+
+
 resource "aws_route53_zone" "primary" {
-  name = "devopsdeployed.com"
+  name = "tick.works"
 }
 
 resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "devopsdeployed.com"
+  name    = "tick.works"
   type    = "A"
 
   alias {
@@ -202,19 +226,71 @@ resource "aws_route53_record" "root" {
   }
 }
 
-resource "aws_db_instance" "db_instance" {
-  allocated_storage = 20
-  # This allows any minor version within the major engine_version
-  # defined below, but will also result in allowing AWS to auto
-  # upgrade the minor version of your DB. This may be too risky
-  # in a real production environment.
-  auto_minor_version_upgrade = true
-  storage_type               = "standard"
-  engine                     = "postgres"
-  engine_version             = "12"
-  instance_class             = "db.t2.micro"
-  name                       = "mydb"
-  username                   = "foo"
-  password                   = "foobarbaz"
-  skip_final_snapshot        = true
+
+# This allows any minor version within the major engine_version
+# defined below, but will also result in allowing AWS to auto
+# upgrade the minor version of your DB. This may be too risky
+# in a real production environment.
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier      = "aurora-postgres-cluster"
+  engine                  = "aurora-postgresql"
+  engine_version          = "13.7" # Adjust to the latest version or the version you need
+  master_username         = "foo"
+  master_password         = "foobarbaz"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.aurora_sg.id]
+  storage_encrypted       = false # Set to true if you need encryption in production
+  backup_retention_period = 1     # Short retention for development
+}
+
+# Aurora Cluster Instance
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  identifier           = "aurora-postgres-instance-1"
+  cluster_identifier   = aws_rds_cluster.aurora_cluster.id
+  instance_class       = "db.t3.medium" # Use smaller instances for development
+  engine               = "aurora-postgresql"
+  engine_version       = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible  = true
+  db_subnet_group_name = aws_db_subnet_group.aurora_subnet_group.name
+}
+
+# Subnet Group for Aurora
+resource "aws_db_subnet_group" "aurora_subnet_group" {
+  name       = "aurora-subnet-group"
+  subnet_ids = [data.aws_subnet.default_subnet_1.id, data.aws_subnet.default_subnet_2.id]
+
+  tags = {
+    Name = "aurora-subnet-group"
+  }
+}
+
+# Security Group for Aurora
+resource "aws_security_group" "aurora_sg" {
+  name        = "aurora-security-group"
+  description = "Allow PostgreSQL access"
+  vpc_id      = data.aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Adjust for your security needs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Outputs for ease of use
+output "aurora_endpoint" {
+  value = aws_rds_cluster.aurora_cluster.endpoint
+}
+
+output "aurora_reader_endpoint" {
+  value = aws_rds_cluster.aurora_cluster.reader_endpoint
 }
